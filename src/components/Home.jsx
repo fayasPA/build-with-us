dc8ddd06d74a7c42985f126061c84d325ce9fd77
fayasPA <fayasmuthalib@gmail.com>
2024-09-28 02:10:43 +0530

    updated business

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -31,10 +31,10 @@ const Home = () => {
 		<div className="h-screen flex flex-col">
 			<Navbar />
 			<section className="flex-1 bg-black text-darkWhite flex" data-scroll-section>
-				<div className="h-full w-[75%]">
+				<div className="h-full w-[75%] pl-2 md:pl-0">
 					<div className="h-full flex flex-col justify-center lg:justify-start ">
 						<div className="w-full flex items-center justify-start ">
-							<div className="text-center" style={{ fontSize: 'clamp(2rem, 6vw, 20rem)', lineHeight: '1' }}>
+							<div className="text-center" style={{ fontSize: 'clamp(1rem, 6vw, 20rem)' }}>
 								<div id="text1" className="inline-block" data-scroll data-scroll-speed="0.1">
 									<span className="text-hover">E</span>
 									<span className="text-hover">N</span>
@@ -52,16 +52,23 @@ const Home = () => {
 							</div>
 						</div>
 
-						<div className="my-[-1rem] sm:my-[-1.5rem] md:my-[-2.5rem] lg:my-[-4.5rem] w-full flex justify-start">
+						<div className="w-full flex justify-start">
+						{/* <div className="my-[-1rem] sm:my-[-1.5rem] md:my-[-2.5rem] lg:my-[-4.5rem] w-full flex justify-start"> */}
 							<div
 								id="text3"
 								className="text-[30cqw] text-center"
-								style={{ fontSize: 'clamp(10rem, 30vw, 30rem)', lineHeight: '1' }}
+								style={{ fontSize: 'clamp(3rem, 10vw, 15rem)', lineHeight: '.4' }}
+								// style={{ fontSize: 'clamp(10rem, 30vw, 30rem)', lineHeight: '1' }}
 								data-scroll
 								data-scroll-speed="-0.1">
-								<span className="text-hover">W</span>
-								<span className="text-hover">E</span>
 								<span className="text-hover">B</span>
+								<span className="text-hover">U</span>
+								<span className="text-hover">S</span>
+								<span className="text-hover">I</span>
+								<span className="text-hover">N</span>
+								<span className="text-hover">E</span>
+								<span className="text-hover">S</span>
+								<span className="text-hover">S</span>
 							</div>
 						</div>
 
@@ -69,7 +76,7 @@ const Home = () => {
 							<div
 								id="text4"
 								className="text-center"
-								style={{ fontSize: 'clamp(3rem, 10vw, 15rem)', lineHeight: '1' }}
+								style={{ fontSize: 'clamp(1rem, 8vw, 15rem)', lineHeight: '1.5' }}
 								data-scroll
 								data-scroll-speed="-0.1">
 								<span className="text-hover">S</span>
@@ -85,7 +92,7 @@ const Home = () => {
 						</div>
 					</div>
 				</div>
-				<div id="text-right-el" className=" vertical-text flex justify-center lg:justify-start text-xl lg:text-2xl text-center flex-wrap">
+				<div id="text-right-el" className=" vertical-text flex flex-1 justify-center items-center md:items-start lg:justify-start text-base md:text-xl lg:text-2xl text-center flex-wrap">
 					<p>Website dev | App Dev | design | AI | Chatbots</p>
 				</div>
 			</section>
